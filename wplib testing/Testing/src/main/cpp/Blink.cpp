6d7ace65a2842d7260cf5044d86a9ac1dccc96a1
Acloran <andrew@cloran.com>
2020-08-16 18:03:59 -0500

    Use pin 2, not 0 or 1

--- wplib testing/Testing/src/main/cpp/Blink.cpp
+++ wplib testing/Testing/src/main/cpp/Blink.cpp
@@ -6,10 +6,10 @@ int main()
     pinMode(0, OUTPUT);
     for (int i = 0; i < 10; i++)
     {
-        digitalWrite(0, HIGH);
+        digitalWrite(2, HIGH);
         delay(1000);
         std::cout << "on";
-        digitalWrite(0, LOW);
+        digitalWrite(2, LOW);
         delay(1000);
         std::cout << "off"; //test
     }

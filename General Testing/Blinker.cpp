57102b7b011554cb0cf832d8acebadbb685ab991
Acloran <32380754+Acloran@users.noreply.github.com>
2020-08-16 18:30:06 -0500

    Create Blinker.cpp

---
+++ General Testing/Blinker.cpp
@@ -0,0 +1,24 @@
+#include <iostream>		// Include all needed libraries here
+#include <wiringPi.h>
+
+using namespace std;		// No need to keep using “std”
+
+int main()
+{
+wiringPiSetup();			// Setup the library
+pinMode(2, OUTPUT);		// Configure GPIO0 as an output
+pinMode(3, INPUT);		// Configure GPIO1 as an input
+
+// Main program loop
+while(1)
+{
+	// Button is pressed if digitalRead returns 0
+	if(digitalRead(1) == 1)
+{	
+	// Toggle the LED
+	digitalWrite(0, !digitalRead(0));
+delay(500); 	// Delay 500ms
+}
+}
+	return 0;
+}

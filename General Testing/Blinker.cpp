516ed7c85586877f9238776e230202cdd2e229f8
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-08-16 19:07:15 -0500

    indented

--- General Testing/Blinker.cpp
+++ General Testing/Blinker.cpp
@@ -7,20 +7,20 @@ using namespace std;		// No need to keep using “std”
 
 int main()
 {
-wiringPiSetup();			// Setup the library
-pinMode(2, OUTPUT);		// Configure GPIO0 as an output
-pinMode(3, INPUT);		// Configure GPIO1 as an input
+	wiringPiSetup();			// Setup the library
+	pinMode(2, OUTPUT);		// Configure GPIO0 as an output
+	pinMode(3, INPUT);		// Configure GPIO1 as an input
 
-// Main program loop
-while(1)
-{
-	// Button is pressed if digitalRead returns 0
-	if(digitalRead(1) == 1)
-{	
-	// Toggle the LED
-	digitalWrite(0, !digitalRead(0));
-delay(500); 	// Delay 500ms
-}
-}
+	// Main program loop
+	while(true)
+	{
+		// Button is pressed if digitalRead returns 0
+		if(digitalRead(1) == 1)
+		{	
+			// Toggle the LED
+			digitalWrite(0, !digitalRead(0));
+			delay(500); 	// Delay 500ms
+		}
+	}
 	return 0;
 }

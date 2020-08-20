79062277d7825379c70e4362a2277b1cd4a08c6a
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-08-20 12:19:39 -0500

    better indentaition

--- General Testing/Motor/Motor.cpp
+++ General Testing/Motor/Motor.cpp
@@ -3,36 +3,36 @@
 
 
     
-        Motor::Motor(int dirForwardPinLoc, int dirBackwardPinLoc, int powerPinLoc)
-            : dirForwardPin{dirForwardPinLoc}, dirBackwardPin{dirBackwardPinLoc}, powerPin{powerPinLoc}
-        {
-            wiringPiSetup();
-            pinMode(dirForwardPin, OUTPUT);
-            pinMode(dirBackwardPin, OUTPUT);
-            pinMode(powerPin, PWM_OUTPUT);
-        }
+Motor::Motor(int dirForwardPinLoc, int dirBackwardPinLoc, int powerPinLoc)
+    : dirForwardPin{dirForwardPinLoc}, dirBackwardPin{dirBackwardPinLoc}, powerPin{powerPinLoc}
+{
+    wiringPiSetup();
+    pinMode(dirForwardPin, OUTPUT);
+    pinMode(dirBackwardPin, OUTPUT);
+    pinMode(powerPin, PWM_OUTPUT);
+}
 
-        void Motor::setPower(double power)
-        {
-            if (power < 0)
-            {
-                //make motor spin backwards
-                digitalWrite(dirForwardPin, LOW);
-                digitalWrite(dirBackwardPin, HIGH);
+void Motor::setPower(double power)
+{
+    if (power < 0)
+    {
+        //make motor spin backwards
+        digitalWrite(dirForwardPin, LOW);
+        digitalWrite(dirBackwardPin, HIGH);
 
-                //give power to motor adjusted (usually from 0 to 1024), negate as power was initially negative
-                double adjustedPower = -1 * power * 1024.0;
-                pwmWrite(powerPin, adjustedPower);
-            }
+        //give power to motor adjusted (usually from 0 to 1024), negate as power was initially negative
+        double adjustedPower = -1 * power * 1024.0;
+        pwmWrite(powerPin, adjustedPower);
+    }
 
-            else
-            { //power > 0
-                //make motor spin forwards
-                digitalWrite(dirForwardPin, HIGH);
-                digitalWrite(dirBackwardPin, LOW);
+    else
+    { //power > 0
+        //make motor spin forwards
+        digitalWrite(dirForwardPin, HIGH);
+        digitalWrite(dirBackwardPin, LOW);
 
-                //give power to motor adjusted (usually from 0 to 1024)
-                double adjustedPower = power * 1024.0;
-                pwmWrite(powerPin, adjustedPower);
-            }
-        }
+        //give power to motor adjusted (usually from 0 to 1024)
+        double adjustedPower = power * 1024.0;
+        pwmWrite(powerPin, adjustedPower);
+    }
+}

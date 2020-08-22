1064a8e0e474773719348c7b1772a385c5b2c94d
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-08-22 12:43:25 -0500

    better indentation

--- General Testing/LED/blink.cpp
+++ General Testing/LED/blink.cpp
@@ -15,21 +15,27 @@ int main()
     pinMode(GPIO_NUM, OUTPUT);
     while (true)
     {
-	for (i=0;i<3;i++)
-		do_blink(DELAY_DOT);
-	delay(DELAY_GAP);
-	for (i=0;i<3;i++)
-		do_blink(DELAY_DASH);
-	delay(DELAY_GAP);
-	for (i=0;i<3;i++)
-		do_blink(DELAY_DOT);
-	delay(DELAY_GAP<<1);
+        for (i=0;i<3;i++)
+        {
+            do_blink(DELAY_DOT);
+        }
+        delay(DELAY_GAP);
+        for (i=0;i<3;i++)
+        {
+            do_blink(DELAY_DASH);
+        }
+        delay(DELAY_GAP);
+        for (i=0;i<3;i++)
+        {
+            do_blink(DELAY_DOT);
+        }
+        delay(DELAY_GAP<<1);
     }
 }
 
 void do_blink(int length)
 {
-        digitalWrite(GPIO_NUM, HIGH);
+    digitalWrite(GPIO_NUM, HIGH);
 	delay(length);
 	digitalWrite(GPIO_NUM, LOW);
 	delay(length);

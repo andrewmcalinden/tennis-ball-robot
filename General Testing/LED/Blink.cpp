e1e6e0b92f390fc1a9d39dc391d5ff3824dc8d27
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-08-21 19:30:04 -0500

    changed pin numbs for test

--- General Testing/LED/Blink.cpp
+++ General Testing/LED/Blink.cpp
@@ -6,10 +6,10 @@ int main()
     pinMode(0, OUTPUT);
     for (int i = 0; i < 10; i++)
     {
-        digitalWrite(2, HIGH);
+        digitalWrite(0, HIGH);
         delay(1000);
         std::cout << "on";
-        digitalWrite(2, LOW);
+        digitalWrite(0, LOW);
         delay(1000);
         std::cout << "off"; //test
     }

dbbb311c992a9e2e7d3803c514499d993e19ef4f
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-08-23 14:31:02 -0500

    lol

--- General Testing/Motor/motorTest.cpp
+++ General Testing/Motor/motorTest.cpp
@@ -18,7 +18,7 @@ int main()
         if (digitalRead(BUTTON_IN) == 0)
         {
             power += 0.05;
-            delay(50)
+            delay(50);
         }
         if (power > 1){
             power = 0;

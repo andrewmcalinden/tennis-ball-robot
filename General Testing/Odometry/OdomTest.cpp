0ffbb7029639a4134c3d6d127773c0129f14b4d2
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-09-04 09:22:31 -0500

    new update pos test

--- General Testing/Odometry/OdomTest.cpp
+++ General Testing/Odometry/OdomTest.cpp
@@ -6,12 +6,13 @@ int main()
     double leftReading = 0;
     double rightReading = 0;
 
-    for (int i = 0; i < 100; i++){
+    for (int i = 0; i < 1000; i++)
+    {
         //want to end at 60, so increase by .6
-        leftReading += .82;
+        leftReading += .12;
 
         //want to end at 40, so increase by .4
-        rightReading -= .3;
+        rightReading += .08;
 
         updatePos(leftReading, rightReading);
         //std::cout << getX() << ", " << getY() << " " << getHeading() << "\n";

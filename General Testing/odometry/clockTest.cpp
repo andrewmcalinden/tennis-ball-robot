41cc001c2f4fc2aaf590fdfc78ed8a1ac37031b4
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-09-22 22:20:39 -0500

    clocktest

---
+++ General Testing/odometry/clockTest.cpp
@@ -0,0 +1,10 @@
+#include <ctime>
+int main()
+{
+    std::clock_t timer;
+    timer = std::clock();
+    while(true)
+    {
+        std::cout << ((std::clock() - timer) / (double)CLOCKS_PER_SEC);
+    }
+}

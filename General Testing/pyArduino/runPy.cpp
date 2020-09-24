926a9a23aba10eb6295702a666ef563761d06348
Acloran <andrew@cloran.com>
2020-09-24 05:36:27 -0900

    used some cool stuff to make more serial stuff work

--- General Testing/pyArduino/runPy.cpp
+++ General Testing/pyArduino/runPy.cpp
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+string exec(string command) {
+   char buffer[128];
+   string result = "";
+
+   // Open pipe to file
+   FILE* pipe = popen(command.c_str(), "r");
+   if (!pipe) {
+      return "popen failed!";
+   }
+
+   // read till end of process:
+   while (!feof(pipe)) {
+
+      // use buffer to read and add to result
+      if (fgets(buffer, 128, pipe) != NULL)
+         result += buffer;
+   }
+
+   pclose(pipe);
+   return result;
+}
 
 int main (){
-	system("cd");
-	system("ls");
+	string output = exec("ls");
+	cout << output;
 }

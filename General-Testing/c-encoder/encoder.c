29d01e69cfce60cbadbbddc3372a0f507e7cda24
texas-scot <texas-scot@users.noreply.github.com>
2020-10-10 16:49:26 -0500

    More python issues

--- General-Testing/c-encoder/encoder.c
+++ General-Testing/c-encoder/encoder.c
@@ -137,9 +137,9 @@ char getReading( void )
   {
     fprintf( stderr, "Called getReading() without a successful startReading() call. Exiting..." );
   }
-  print("\nWaiting on produced");
+  printf("\nWaiting on produced");
   sem_wait(&produced);
-  print("\nGot produced");
+  printf("\nGot produced");
   char value = buffer[START];
   START = (START+1) % BUFFER_SIZE;
   return value;
@@ -150,7 +150,7 @@ void continueReading( void )
   if( RUNNING )
   {
     sem_post(&consumed);
-    print("\nPosted consumed");
+    printf("\nPosted consumed");
   }
 }
 

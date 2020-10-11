b54f5157644b7d900d69fac4f8cc3efb9e1aa83e
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-10-11 13:41:58 -0500

    can while loops run?

--- General-Testing/c-encoder/example.c
+++ General-Testing/c-encoder/example.c
@@ -20,42 +20,42 @@ int main( )
   printf("%d\n", value);
   while( 1 )
   {
-    char reading = getReading();
-    printf("\n%d\n", (int) reading);
-    /* ["critical section"]
-     * Do whatever you like with the reading here
-     * For now we are just printing a knob value.
-     * Clockwise - is positive, increments the value
-     * Counterclockwise - decrements the value
-     * Pressing knob  - resets the value to 0 
-     * If buffering is enabled, any activity will be
-     * read into a buffer (until full) while in this
-     * critical section. If buffering is not enabled
-     * then all values being read in while in this 
-     * critical section are ignored, until the call
-     * to continueReading().
-     */
-    if( reading == CLOCKWISE_STEP )
-    {
-      value++;
-    }
-    else if( reading == COUNTER_CLOCKWISE_STEP )
-    {
-      value--;
-    }
-    else if( reading == PUSHED )
-    {
-      value = 0;
-    }
-    else if( reading == PUSHED_AND_HELD )
-    {
-      value = 100;
-    }
-    printf( "%d\n", value );
+    // char reading = getReading();
+    // printf("\n%d\n", (int) reading);
+    // /* ["critical section"]
+    //  * Do whatever you like with the reading here
+    //  * For now we are just printing a knob value.
+    //  * Clockwise - is positive, increments the value
+    //  * Counterclockwise - decrements the value
+    //  * Pressing knob  - resets the value to 0 
+    //  * If buffering is enabled, any activity will be
+    //  * read into a buffer (until full) while in this
+    //  * critical section. If buffering is not enabled
+    //  * then all values being read in while in this 
+    //  * critical section are ignored, until the call
+    //  * to continueReading().
+    //  */
+    // if( reading == CLOCKWISE_STEP )
+    // {
+    //   value++;
+    // }
+    // else if( reading == COUNTER_CLOCKWISE_STEP )
+    // {
+    //   value--;
+    // }
+    // else if( reading == PUSHED )
+    // {
+    //   value = 0;
+    // }
+    // else if( reading == PUSHED_AND_HELD )
+    // {
+    //   value = 100;
+    // }
+    // printf( "%d\n", value );
     printf("dog\n");
     /* ["critical section end"] */
 
-    continueReading(); 
+    //continueReading(); 
   }
 
   return 0;

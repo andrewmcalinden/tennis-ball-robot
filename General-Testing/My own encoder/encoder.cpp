27d86dbae58809052862bcd02ebb395d7b21546e
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-10-18 15:37:49 -0500

    removes *

--- General-Testing/My own encoder/encoder.cpp
+++ General-Testing/My own encoder/encoder.cpp
@@ -53,9 +53,9 @@ void Encoder::update()
     }
 }
 
-static void updateCallback(void* this_pointer)
+static void updateCallback(void)
 {
-    Encoder* self = static_cast<Encoder*>(this_pointer);
+    Encoder self = static_cast<Encoder>(this_pointer);
     self->update();
 }
 

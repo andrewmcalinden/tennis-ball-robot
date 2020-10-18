71195f999bb0ff733d8da833dedec5cb1938faa1
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-10-18 15:11:11 -0500

    removed update(void)

--- General-Testing/My own encoder/encoder.cpp
+++ General-Testing/My own encoder/encoder.cpp
@@ -17,11 +17,11 @@ Encoder::Encoder(int pinALoc, int pinBLoc)
     {
         state |= 2;
     }
-    wiringPiISR(pinA, INT_EDGE_BOTH, &Encoder::updateCallback, &this));
-    wiringPiISR(pinB, INT_EDGE_BOTH, &Encoder::updateCallback, &this));
+    wiringPiISR(pinA, INT_EDGE_BOTH, &Encoder::updateCallback, this));
+    wiringPiISR(pinB, INT_EDGE_BOTH, &Encoder::updateCallback, this));
 }
 
-void Encoder::update(void)
+void Encoder::update()
 {
     unsigned char currentState = state & 3;
     if (digitalRead(7))

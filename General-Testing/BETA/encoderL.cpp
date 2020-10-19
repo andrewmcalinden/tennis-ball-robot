cd8a085b68d3a0d05c69eccc816edf2ce3d5e83e
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-10-19 15:04:00 -0500

    Revert "added class name"
    This reverts commit 40ab39e167f586f9456c16a7454031d63e6ad4b4.

--- General-Testing/BETA/encoderL.cpp
+++ General-Testing/BETA/encoderL.cpp
@@ -1,22 +1,22 @@
 #include "encoderL.h"
 EncoderL::EncoderL()
 {
-    EncoderL::pinA = 2;
-    EncoderL::pinB = 3;
-    EncoderL::position = 0;
-    EncoderL::state = 0;
+    pinA = 2;
+    pinB = 3;
+    position = 0;
+    state = 0;
 
     wiringPiSetup();
-    pinMode(EncoderL::pinA, INPUT);
-    pinMode(EncoderL::pinB, INPUT);
+    pinMode(pinA, INPUT);
+    pinMode(pinB, INPUT);
 
-    if (digitalRead(EncoderL::pinA))
+    if (digitalRead(pinA))
     {
-        EncoderL::state |= 1;
+        state |= 1;
     }
-    if (digitalRead(EncoderL::pinB))
+    if (digitalRead(pinB))
     {
-        EncoderL::state |= 2;
+        state |= 2;
     }
 
     wiringPiISR(pinA, INT_EDGE_BOTH, &update);
@@ -25,37 +25,37 @@ EncoderL::EncoderL()
 
 void EncoderL::update()
 {
-    unsigned char currentState = EncoderL::state & 3;
-    if (digitalRead(EncoderL::pinA))
+    unsigned char currentState = state & 3;
+    if (digitalRead(pinA))
     {
         currentState |= 4;
     }
-    if (digitalRead(EncoderL::pinB))
+    if (digitalRead(pinB))
     {
         currentState |= 8;
     }
 
-    EncoderL::state = currentState >> 2;
+    state = currentState >> 2;
 
     if (currentState == 1 || currentState == 7 || currentState == 8 || currentState == 14)
     {
-        EncoderL::position += 1;
+        position += 1;
     }
     else if (currentState == 2 || currentState == 4 || currentState == 11 || currentState == 13)
     {
-        EncoderL::position -= 1;
+        position -= 1;
     }
     else if (currentState == 3 || currentState == 12)
     {
-        EncoderL::position += 2;
+        position += 2;
     }
     else if (currentState == 6 || currentState == 9)
     {
-        EncoderL::position -= 2;
+        position -= 2;
     }
 }
 
 int EncoderL::read()
 {
-    return EncoderL::position;
+    return position;
 }

240f0114c21cb584f6e780270472e9b099eb85a5
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-10-19 19:26:55 -0500

    better formatting

--- General-Testing/odometry/odomTest.cpp
+++ General-Testing/odometry/odomTest.cpp
@@ -10,11 +10,11 @@ int main()
     while (true)
     {
         updatePos(EncoderL::read(), EncoderR::read());
-        std::cout << "\nX: ";
+        std::cout << "\nX:";
         printf("%.2f", getX());
-        std::cout << "  Y: ";
+        std::cout << "  Y:";
         printf("%.2f", getY());
-        std::cout << "  Heading: ";
+        std::cout << "  Heading:";
         printf("%.2f", getHeading());
     }
 }

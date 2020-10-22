d056c195c1042866c864c40dcca71d9fe4ad8ba0
Acloran <32380754+Acloran@users.noreply.github.com>
2020-10-22 11:22:47 -0500

    Update basicInchTest.cpp

--- General-Testing/BETA/basicInchTest.cpp
+++ General-Testing/BETA/basicInchTest.cpp
@@ -10,7 +10,7 @@ int main()
     EncoderL::begin();
     while(true)
     {
-        std::cout << "Left: " << (EncoderL::read()/1440)*2.04*M_PI) << "\n";
+        std::cout << "Left: " << (EncoderL::read()/1440)*2.04*M_PI << "\n";
         std::cout << "Boomer \n";
     }
 }

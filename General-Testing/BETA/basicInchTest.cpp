4bee1e92304f6f0d23761ee92887d1c811b328eb
Acloran <32380754+Acloran@users.noreply.github.com>
2020-10-22 10:58:21 -0500

    Create basicInchTest.cpp

---
+++ General-Testing/BETA/basicInchTest.cpp
@@ -0,0 +1,15 @@
+#include "encoderL.h"
+#include<iostream>
+#include<cmath>
+
+int EncoderL::position = 0;
+unsigned char EncoderL::state = 0;
+
+int main()
+{
+    EncoderL::begin();
+    while(true)
+    {
+        std::cout << "Left: " << (EncoderL::read()/1440)*2.04 * M_PI << "\n";
+    }
+}

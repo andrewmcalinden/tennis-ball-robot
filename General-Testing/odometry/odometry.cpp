b034c156aec759a3b8ac4037d85fabba6b5095c1
Acloran <32380754+Acloran@users.noreply.github.com>
2020-10-22 11:58:15 -0500

    Update odometry.cpp

--- General-Testing/odometry/odometry.cpp
+++ General-Testing/odometry/odometry.cpp
@@ -71,7 +71,7 @@ void updatePos(double leftReading, double rightReading)
     double movement = (leftChange + rightChange) / 2.0; // total change in movement by robot (dx)
     std::cout << "Movement: " << movement;
     std::cout << "Abs: " << abs(movement) << "\n";
-    totalDist += abs(movement);
+    totalDist += movement;
     double dTheta = angleChangeRad;
 
     double sinTheta = sin(dTheta);

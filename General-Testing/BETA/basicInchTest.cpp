00f3dcf5e51d00d69c8608c660451421a5045a09
Acloran <32380754+Acloran@users.noreply.github.com>
2020-10-22 11:16:40 -0500

    XCooco

--- General-Testing/BETA/basicInchTest.cpp
+++ General-Testing/BETA/basicInchTest.cpp
@@ -10,6 +10,6 @@ int main()
     EncoderL::begin();
     while(true)
     {
-        std::cout << "Left: " << (EncoderL::read()/1440)*2.04*M_PI) << "\n";
+        std::cout << "Left: " << (EncoderL::read()/1440)*2.04*M_PI) << "\n"; //ysy
     }
 }

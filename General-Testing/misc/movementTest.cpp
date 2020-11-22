0d68ceca331d747dfc6798e73f0061f55231214d
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-21 18:22:42 -0600

    new test

--- General-Testing/misc/movementTest.cpp
+++ General-Testing/misc/movementTest.cpp
@@ -2,14 +2,13 @@
 #include <cmath>
 int main()
 {
-    double initialX = 2;
-    double initialY = 1;
-    double initialHeadingRad = (M_PI / 180.0) * -45.0;
+    double initialX = 3;
+    double initialY = -2;
+    double initialHeadingRad = (M_PI / 180.0) * 160;
 
-    //flipped because if you actually draw the triangle it works out
-    double additionalX = 20 * -sin(initialHeadingRad);
-    double additionalY = 20 * -cos(initialHeadingRad);
-    std::cout << additionalY;
+    //flipped signs and trig because if you actually draw the triangle it works out
+    double additionalX = 60 * -sin(initialHeadingRad);
+    double additionalY = 60 * -cos(initialHeadingRad);
 
     double finalX = initialX + additionalX;
     double finalY = initialY + additionalY;

f11aa77a4cdf4f0b2d122300fd0d678042ca2a33
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-21 20:31:57 -0600

    maybe it was easy all along?

--- General-Testing/misc/newPosTest.cpp
+++ General-Testing/misc/newPosTest.cpp
@@ -7,8 +7,8 @@ int main()
     double initialHeadingRad = (M_PI / 180.0) * 160;
 
     //flipped signs and trig because if you actually draw the triangle it works out
-    double additionalX = 60 * -sin(initialHeadingRad);
-    double additionalY = 60 * -cos(initialHeadingRad);
+    double additionalX = 60 * cos(initialHeadingRad);
+    double additionalY = 60 * sin(initialHeadingRad);
 
     double finalX = initialX + additionalX;
     double finalY = initialY + additionalY;

4e8045cbd8856b2a736295533d9b6d8a85f116f3
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-23 17:26:26 -0600

    new pin

--- General-Testing/motor/motorButtonControl.cpp
+++ General-Testing/motor/motorButtonControl.cpp
@@ -1,7 +1,7 @@
 #include "motor.h"
 
 #define DIRECTION_PIN_FORWARD 2
-#define POWER_PIN 23 //GPIO 13
+#define POWER_PIN 10 //GPIO 13
 #define BUTTON_IN 6 //GPIO 25
 
 int main()
@@ -18,7 +18,8 @@ int main()
             power += 0.01;
             delay(15);
         }
-        if (power > 1){
+        if (power > 1)
+        {
             power = 0;
         }
         //std::cout << power << "\n";

8d0924276f6c5dbe74121a0c94541cf192bd57d0
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-23 17:59:22 -0600

    raw pwm test

--- General-Testing/motor/pwmTest.cpp
+++ General-Testing/motor/pwmTest.cpp
@@ -2,26 +2,21 @@
 #include <iostream>
 #define DIRECTION_PIN_FORWARD 2
 #define POWER_PIN 10 //GPIO 13
-#define BUTTON_IN 6 //GPIO 25
 
 int main()
 {
     wiringPiSetup();
-    pinMode(BUTTON_IN, INPUT);
-
-    //Motor m(DIRECTION_PIN_FORWARD, POWER_PIN);
+    Motor m(DIRECTION_PIN_FORWARD, POWER_PIN);
     double power = 0;
     while (true)
     {
-        if (digitalRead(BUTTON_IN) == 0)
-        {
-            power += 0.01;
-        }
+        power += 0.01;
+        delay(15);
         if (power > 1)
         {
             power = 0;
         }
         std::cout << power << "\n";
-        //m.setPower(power);
+        m.setPower(power);
     }
 }

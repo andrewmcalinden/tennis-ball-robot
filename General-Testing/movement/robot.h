322542efa1e37589b51bf590893f6b67811fa130
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-22 22:49:42 -0600

    just comment it out

--- General-Testing/movement/robot.h
+++ General-Testing/movement/robot.h
@@ -10,7 +10,7 @@ class Robot
     //Encoders are static
 
     public:
-    Robot(int lMotorDirPin, int lMotorPowerPin, int rMotorDirPin, int rMotorPowerPin, double initialX, double initialY, double initialTheta);
+    //Robot(int lMotorDirPin, int lMotorPowerPin, int rMotorDirPin, int rMotorPowerPin, double initialX, double initialY, double initialTheta);
 
     void goStraight(double inches, double p, double i, double d, double f);
     void turnHeading(double angle); //in degrees

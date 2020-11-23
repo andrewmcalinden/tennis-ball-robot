380f154df414a7954ac316ba111821d3fefa060f
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-22 21:37:26 -0600

    new change

--- General-Testing/odometry/odomTest.cpp
+++ General-Testing/odometry/odomTest.cpp
@@ -3,14 +3,14 @@
 #include "../BETA/encoderL.h"
 #include "../BETA/encoderR.h"
 
-int main()
-{
-    int EncoderL::position = 0;
-    unsigned char EncoderL::state = 0;
+int EncoderL::position = 0;
+unsigned char EncoderL::state = 0;
 
-    int EncoderR::position = 0;
-    unsigned char EncoderR::state = 0;
-    
+int EncoderR::position = 0;
+unsigned char EncoderR::state = 0;
+
+int main()
+{ 
     EncoderL::begin();
     EncoderR::begin();
     while (true)

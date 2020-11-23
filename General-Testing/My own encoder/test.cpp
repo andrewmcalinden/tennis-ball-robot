80dc15c95bb51d6049e339ae60c888c837bf544a
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-23 13:37:43 -0600

    l and r read

--- General-Testing/My own encoder/test.cpp
+++ General-Testing/My own encoder/test.cpp
@@ -3,10 +3,11 @@
 
 int main()
 {
-    Encoder enc(7, 0);
-    Encoder enc(2, 3);
+    Encoder encL(7, 0);
+    Encoder encR(2, 3);
     while (true)
     {
-        std::cout << enc.read() << "\n";
+        std::cout << "lRead: " << encL.read();
+        std::cout << "  rRead: " << encL.read() << "\n";
     }
 }

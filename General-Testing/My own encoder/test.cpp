6fae1458a1ceae64c5513999b8f02274875c14e2
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-23 21:36:32 -0600

    read 2 alone

--- General-Testing/My own encoder/test.cpp
+++ General-Testing/My own encoder/test.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 int main()
 {
-    Encoder enc1(2, 3);
-    //Encoder enc2(0, 7);
+    //Encoder enc1(2, 3);
+    Encoder enc2(0, 7);
     while (true)
     {
-        std::cout << "read1: " << enc1.read(); //<< "  read2: " << enc2.read() << "\n";
+        std::cout /*<< "read1: " << enc1.read() <<*/ "  read2: " << enc2.read() << "\n";
     }
 }

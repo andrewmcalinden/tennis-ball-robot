b4688acb4d37bd7e00dc446bcbebc54af7438f47
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-23 21:40:50 -0600

    3rd encoder

--- General-Testing/My own encoder/test.cpp
+++ General-Testing/My own encoder/test.cpp
@@ -3,9 +3,10 @@
 int main()
 {
     Encoder enc1(0, 7);
-    Encoder enc2(2, 3);
+    Encoder enc2(5, 8);
+    Encoder enc3(2, 3);
     while (true)
     {
-        std::cout << "read1: " << enc1.read() << "  read2: " << enc2.read() << "\n";
+        std::cout << "read1: " << enc1.read() << "  read2: " << enc2.read() << "  read3: " << enc3.read() << "\n";
     }
 }

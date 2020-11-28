5dd3182954c5ec3bcfefe52eaa4ed060d043266b
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-27 21:39:15 -0600

    switch order

--- General-Testing/My own encoder/lookup/experimentalTest.cpp
+++ General-Testing/My own encoder/lookup/experimentalTest.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 int main()
 {
-    ExperimentalEncoder enc1(0, 7);
     ExperimentalEncoder enc2(2, 3);
+    ExperimentalEncoder enc1(0, 7);
     while (true)
     {
         std::cout << "read1: " << enc1.read() << "  read2: " << enc2.read() << "\n";

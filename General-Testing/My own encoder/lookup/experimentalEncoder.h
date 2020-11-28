3e37a67a564bee262b1bcf9489cf85ace9c6c38d
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-27 20:03:30 -0600

    doesnt need to be in class

--- General-Testing/My own encoder/lookup/experimentalEncoder.h
+++ General-Testing/My own encoder/lookup/experimentalEncoder.h
@@ -9,11 +9,9 @@ private:
 
 public:
     ExperimentalEncoder(int pinALoc, int pinBLoc);
-    void update(unsigned char pin);
     int read();
 
     static void callbackMethod();
     void registerCallback();
 
-    void Interrupt0(void);
 };

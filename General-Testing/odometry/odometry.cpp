76cd7df0114a3fd029f14bddb4575c4005ea26ed
Acloran <32380754+Acloran@users.noreply.github.com>
2020-11-28 09:45:41 -0600

    Correct Track Width
    For new setup

--- General-Testing/odometry/odometry.cpp
+++ General-Testing/odometry/odometry.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <cstdlib>
 
-#define TRACKWIDTH 9.25
+#define TRACKWIDTH 16.5
 #define PULSES_PER_REV 1440.0
 #define EPSILON 1.0e-6
 #define LEFT_WHEEL__DIAMETER 2.037

d6616970c54235df7a9992b1505f31dd5b6bf558
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-27 21:40:09 -0600

    change names

--- General-Testing/My own encoder/lookup/experimentalTest.cpp
+++ General-Testing/My own encoder/lookup/experimentalTest.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 int main()
 {
-    ExperimentalEncoder enc2(2, 3);
-    ExperimentalEncoder enc1(0, 7);
+    ExperimentalEncoder enc1(2, 3);
+    ExperimentalEncoder enc2(0, 7);
     while (true)
     {
         std::cout << "read1: " << enc1.read() << "  read2: " << enc2.read() << "\n";

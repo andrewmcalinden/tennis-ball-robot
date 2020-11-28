968f1021b58d34495462675085f666215cedbc70
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-11-27 22:16:57 -0600

    only print if hange

--- General-Testing/My own encoder/lookup/experimentalTest.cpp
+++ General-Testing/My own encoder/lookup/experimentalTest.cpp
@@ -5,9 +5,27 @@ int main()
 {
     ExperimentalEncoder enc1(2, 3);
     ExperimentalEncoder enc2(0, 7);
+
+    int read1 = 0;
+    int pastRead1 = 0;
+
+    int read2 = 0;
+    int pastRead2 = 0;
     while (true)
     {
-        std::cout << "read1: " << enc1.read() << "  read2: " << enc2.read() << "\n";
+        read1 = enc1.read();
+        read2 = enc2.read();
+        if (read1 != pastRead1)
+        {
+            std::cout << "read1: " << read1;
+        }
+        if (read2 != pastRead2)
+        {
+            std::cout << "  read2: " << read2;
+        }
+        std::cout << "\n";
         delay(15);
+        pastRead1 = read1;
+        pastRead2 = read2;
     }
 }

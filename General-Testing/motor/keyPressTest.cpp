96287b018be775720541e8e784ecd053f283b784
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-04 15:16:04 -0600

    Update keyPressTest.cpp

--- General-Testing/motor/keyPressTest.cpp
+++ General-Testing/motor/keyPressTest.cpp
@@ -7,8 +7,8 @@ int main()
     int c = 0;
     std::cout << "xpress";
     while(true){
-     c = getchar();
-    if (c == 97){
+     c = getstring();
+    if (c == "run"){
         std::cout << "x";
     }
     }

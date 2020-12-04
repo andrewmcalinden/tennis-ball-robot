0a830e659ce239e8439a27f78fc6d6457c85e4f4
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-04 10:28:19 -0600

    Create motorTestNoEncoder.cpp

---
+++ General-Testing/motor/motorTestNoEncoder.cpp
@@ -0,0 +1,36 @@
+#include "motor.h"
+#include <iostream>
+#include <wiringPi.h>
+#include<unistd.h>
+
+#define DIRECTION_PIN_FORWARD_LEFT 22
+#define POWER_PIN_LEFT 23
+#define DIRECTION_PIN_FORWARD_RIGHT 16
+#define POWER_PIN_RIGHT 26
+#define BUTTON_IN 6
+
+int main()
+{
+    wiringPiSetup();
+    pinMode(BUTTON_IN, INPUT);
+
+    Motor motorL(DIRECTION_PIN_FORWARD_LEFT, POWER_PIN_LEFT);
+    Motor motorR(DIRECTION_PIN_FORWARD_RIGHT, POWER_PIN_RIGHT);
+    double power = 0;
+    while (true)
+    {
+        while (power < 1){
+        std::cout << power << "\r";
+        motorL.setPower(power);
+        motorR.setPower(power);
+        power += .01;
+        usleep(10000);
+        }
+        while (power > -1){
+        std::cout << power << "\r";
+        motorL.setPower(power);
+        motorR.setPower(power);
+        power -= .01;
+        usleep(10000);
+    }
+}

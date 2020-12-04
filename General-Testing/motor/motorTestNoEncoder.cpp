7144f293e40c7283753970f5ee5fce0d6e5e7030
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-04 13:41:00 -0600

    Update motorTestNoEncoder.cpp

--- General-Testing/motor/motorTestNoEncoder.cpp
+++ General-Testing/motor/motorTestNoEncoder.cpp
@@ -14,25 +14,9 @@ int main()
 
     Motor motorL(DIRECTION_PIN_FORWARD_LEFT, POWER_PIN_LEFT);
     Motor motorR(DIRECTION_PIN_FORWARD_RIGHT, POWER_PIN_RIGHT);
-    double Lpower = 1;
-    double Rpower = -1;
+    double power = -1;
     while (true)
     {
-<<<<<<< HEAD
-        while (Rpower < 1){
-        motorL.setPower(Lpower);
-        motorR.setPower(Rpower);
-        Rpower += .01;
-        Lpower -= .01;
-        usleep(10000);
-        }
-        while (Rpower > -1){
-        motorL.setPower(Lpower);
-        motorR.setPower(Rpower);
-        Rpower -= .01;
-        Lpower += .01;
-        usleep(10000);
-=======
         while (power < 1){
         motorL.setPower(power);
         motorR.setPower(power);
@@ -44,7 +28,6 @@ int main()
         motorR.setPower(power);
         power -= .01;
         usleep(50000);
->>>>>>> parent of fb08e04... Update motorTestNoEncoder.cpp
     }
 }
 }

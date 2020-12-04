4b0a905659edb8e113785eadb59fc700c32284c0
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-04 15:36:20 -0600

    Update keyPressTest.cpp

--- General-Testing/motor/keyPressTest.cpp
+++ General-Testing/motor/keyPressTest.cpp
@@ -1,15 +1,33 @@
-
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
 
+using namespace std;
 
 int main()
 {
-    int c = 0;
-    std::cout << "xpress";
+    std::filebuf fb;
+  if (fb.open ("test.txt",std::ios::in))
+  {
+    std::istream is(&fb);
+    while (is)
+      std::cout << char(is.get());
+    fb.close();
+  
+    const char * c;
+    //c = "";
+    
+    
+    cout << "xpress";
     while(true){
-     c = getline();
-    if (c.equals("run")){
-        std::cout << "x";
+
+        getline(cin, c);
+
+        if ((strcmp(c, "run")==0)){
+            cout << "x";
+        }
+    
     }
-    }
+  }
 }

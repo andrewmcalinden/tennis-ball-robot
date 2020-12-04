e3f256910ab917e856b2c3650e919fd94c7ef892
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-04 13:35:01 -0600

    Update motorTestNoEncoder.cpp

--- General-Testing/motor/motorTestNoEncoder.cpp
+++ General-Testing/motor/motorTestNoEncoder.cpp
@@ -14,19 +14,22 @@ int main()
 
     Motor motorL(DIRECTION_PIN_FORWARD_LEFT, POWER_PIN_LEFT);
     Motor motorR(DIRECTION_PIN_FORWARD_RIGHT, POWER_PIN_RIGHT);
-    double power = 0;
+    double Lpower = 1;
+    double Rpower = 0;
     while (true)
     {
-        while (power < 1){
-        motorL.setPower(power);
-        motorR.setPower(power);
-        power += .01;
+        while (Rpower < 1){
+        motorL.setPower(Lpower);
+        motorR.setPower(Rpower);
+        Rpower += .01;
+        Lpower -= .01;
         usleep(10000);
         }
-        while (power > -1){
-        motorL.setPower(power);
-        motorR.setPower(power);
-        power -= .01;
+        while (Rpower > -1){
+        motorL.setPower(Lpower);
+        motorR.setPower(Rpower);
+        Rpower -= .01;
+        Lpower += .01;
         usleep(10000);
     }
 }

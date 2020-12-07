7fb48b32286968755f396f28d34bfab031731166
Acloran <andrew@cloran.com>
2020-12-07 16:55:07 -0600

    Update motorTestNoEncoder.cpp

--- General-Testing/motor/motorTestNoEncoder.cpp
+++ General-Testing/motor/motorTestNoEncoder.cpp
@@ -11,12 +11,15 @@
 
 using namespace std;
 
+
 int main()
 {
     wiringPiSetup();
 
     Motor motorL(DIRECTION_PIN_FORWARD_LEFT, POWER_PIN_LEFT);
     Motor motorR(DIRECTION_PIN_FORWARD_RIGHT, POWER_PIN_RIGHT);
+    motorL.setPower(0);
+    motorR.setPower(0);
     double power = -1;
     string c = "";
     while (true)
@@ -24,16 +27,16 @@ int main()
         getline(cin, c);
 
         if ((c=="run")){
-            motorL.setPower(1);
-            motorR.setPower(1);
+            motorL.setPower(.5);
+            motorR.setPower(.5);
         }
         if ((c=="stop")){
             motorL.setPower(0);
             motorR.setPower(0);
         }
         if ((c=="reverse")){
-            motorL.setPower(-1);
-            motorR.setPower(-1);
+            motorL.setPower(-.5);
+            motorR.setPower(-.5);
         }
         
     }

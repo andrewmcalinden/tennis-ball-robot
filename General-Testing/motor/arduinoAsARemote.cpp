a96a103b91baac3f71158a7766cd420b6f45a3a2
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-11 13:36:16 -0600

    Priinting time

--- General-Testing/motor/arduinoAsARemote.cpp
+++ General-Testing/motor/arduinoAsARemote.cpp
@@ -23,7 +23,7 @@ int main()
         unsigned int timeGap = micros()-timeInit;
 
         double Lpower = timeGap/2041.0;
-        printf("\nLEFT %.6f",timeGap);
+        printf("\nLEFT: ",micros());
         while(digitalRead(LEFTPIN) == 1){}
         
     }

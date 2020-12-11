1e02316b65c7f37f5a85db6e07efa263ed05a753
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-11 13:42:18 -0600

    Testing and printing time between pulses

--- General-Testing/motor/arduinoAsARemote.cpp
+++ General-Testing/motor/arduinoAsARemote.cpp
@@ -23,7 +23,7 @@ int main()
         unsigned int timeGap = micros()-timeInit;
 
         double Lpower = timeGap/2041.0;
-        cout << timeInit << "\n";
+        cout << timeGap << "\n";
         while(digitalRead(LEFTPIN) == 1){}
         
     }

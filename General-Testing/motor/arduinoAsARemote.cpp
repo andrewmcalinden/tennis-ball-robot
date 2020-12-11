d4b0a6385f5e7ba483cf89c25bc3d6d55fb7f3b6
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-11 13:03:37 -0600

    Deleted a useless file,
     added code to allow the Pi to read incoming pwn signals  from arduino based off a joystick's location.

---
+++ General-Testing/motor/arduinoAsARemote.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "motor.h"
+#include <wiringPi.h>
+
+using namespace std;
+
+#define LEFTPIN 5
+#define RIGHTPIN 6
+
+
+int main()
+{
+    wiringPiSetup();
+
+    pinMode(LEFTPIN, INPUT);
+    pinMode(RIGHTPIN, INPUT);
+
+    while (true){
+        unsigned int timeInit = micros();
+        while(digitalRead(LEFTPIN) == 0||(micros()-timeInit)<=2041){}
+        unsigned int timeGap = micros()-timeInit;
+        double Lpower = timeGap/2041.0
+        while(digitalRead(LEFTPIN) == 1){}
+        cout << Lpower;
+    }
+}
+

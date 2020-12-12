561f110717c4711a6610715b550e75976243ead2
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-12 15:57:57 -0600

    rising?

--- General-Testing/motor/arduinoAsARemote.cpp
+++ General-Testing/motor/arduinoAsARemote.cpp
@@ -52,7 +52,7 @@ int main()
 
     
     //wiringPiISR (LEFTPIN, INT_EDGE_RISING, &rwmReaderLRise);
-    wiringPiISR (LEFTPIN, INT_EDGE_FALLING, &rwmReaderLFall);
+    wiringPiISR (LEFTPIN, INT_EDGE_RISING, &rwmReaderLFall);
     while (true){
         //timeInit = micros(); //gets a time baseline
         //baseTime = micros();

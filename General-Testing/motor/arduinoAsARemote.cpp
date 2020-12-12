8d9fa827a0f723f650b3b7e949786a039fbc509b
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-12 16:12:30 -0600

    10sec

--- General-Testing/motor/arduinoAsARemote.cpp
+++ General-Testing/motor/arduinoAsARemote.cpp
@@ -58,7 +58,7 @@ int main()
         //timeInit = micros(); //gets a time baseline
         //baseTime = micros();
         
-        delay(4000);
+        delay(10000);
         cout << count << "\n";
         //delay(2);
         //wiringPiISR (LEFTPIN, INT_EDGE_FALLING, &rwmReaderLFall);

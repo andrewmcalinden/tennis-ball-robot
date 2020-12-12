7d94416150ace8120de205db8235af2fded84aab
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-12 15:05:17 -0600

    bare

--- General-Testing/motor/arduinoAsARemote.cpp
+++ General-Testing/motor/arduinoAsARemote.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 double Lpower = 0;
 double Rpower = 0;
-unsigned int timeInit = 0;
-unsigned int baseTime = 0;
+volatile int timeInit = 0;
+volatile int baseTime = 0;
 
 double superMap(double x, double in_min, double in_max, double out_min, double out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -19,7 +19,7 @@ double superMap(double x, double in_min, double in_max, double out_min, double o
 void rwmReaderLRise(){
     
     baseTime = micros();
-
+    cout << baseTime;
     //Lpower = superMap(timeGap, 15, 2036, -1, 1);
     //baseTime = micros();
 }
@@ -29,7 +29,7 @@ void rwmReaderLFall(){
     
     printf("\nLEFT: %.2f", Lpower);
     //printf("\nLEFT: %.2f", Lpower);
-    Lpower = superMap(timeGap, 15, 2036, -1, 1);
+    //Lpower = superMap(timeGap, 15, 2036, -1, 1);
     //baseTime = micros();
 }
 volatile int count = 0;
@@ -47,8 +47,8 @@ int main()
     while (true){
         //timeInit = micros(); //gets a time baseline
         //baseTime = micros();
-        wiringPiISR (LEFTPIN, INT_EDGE_RISING, &counter);
-        cout << count;
+        wiringPiISR (LEFTPIN, INT_EDGE_RISING, &rwmReaderLRise);
+        //cout << count;
         //wiringPiISR (LEFTPIN, INT_EDGE_FALLING, &rwmReaderLFall);
         
         //printf("\nLEFT: %.2f", Lpower);

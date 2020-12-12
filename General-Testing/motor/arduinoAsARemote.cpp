5d14616817cc3703b60f0c4732428679e499b1ce
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-12 15:31:41 -0600

    maybe?

--- General-Testing/motor/arduinoAsARemote.cpp
+++ General-Testing/motor/arduinoAsARemote.cpp
@@ -23,7 +23,7 @@ double superMap(double x, double in_min, double in_max, double out_min, double o
 void rwmReaderLRise(){
     
     baseTime = micros();
-    printf(count , ": " , baseTime , "\n");
+    printf("counter: ", count , ": " , baseTime , "\n");
     //Lpower = superMap(timeGap, 15, 2036, -1, 1);
     //baseTime = micros();
     delay(2);

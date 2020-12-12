369b82c02749c9a95fdadf2269165f7a31a3ceae
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-12 12:45:30 -0600

    commented out

--- General-Testing/motor/arduinoAsARemote.cpp
+++ General-Testing/motor/arduinoAsARemote.cpp
@@ -14,7 +14,7 @@ double superMap(double x, double in_min, double in_max, double out_min, double o
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void rwmReaderL(unsigned int baseTime){
+void rwmReaderL(/*unsigned int baseTime*/){
     unsigned int timeGap = micros()-baseTime;
     Lpower = superMap(timeGap, 15, 2036, -1, 1);
 }
@@ -29,7 +29,7 @@ int main()
     while (true){
         unsigned int timeInit = micros(); //gets a time baseline
 
-        wiringPiISR (LEFTPIN, INT_EDGE_RISING,  &rwmReaderL(timeInit));
+        wiringPiISR (LEFTPIN, INT_EDGE_RISING,  &rwmReaderL);
 
         printf("\nLEFT: %.2f", Lpower);
         //while(digitalRead(LEFTPIN) == 1){}

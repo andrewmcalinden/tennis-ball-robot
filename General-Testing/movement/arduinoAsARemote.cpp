43aa732353c5c00b728ee5959b857f97c3972669
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-12 19:04:05 -0600

    Trying to implement mulptiple staes of the interrupt

--- General-Testing/movement/arduinoAsARemote.cpp
+++ General-Testing/movement/arduinoAsARemote.cpp
@@ -16,6 +16,7 @@ volatile int timeInit = 0;
 volatile int baseTime = 0;
 volatile int count = 1;
 volatile int countoo = 1;
+bool lHigh = 1;
 
 double superMap(double x, double in_min, double in_max, double out_min, double out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -30,8 +31,11 @@ void rwmReaderLRise(){
     //delay(2);
     //count++;
 }
-void rwmReaderLFall(){
-
+void rwmReaderL(){
+    if(!lHigh){
+    baseTime = micros();
+    }
+    else{
     unsigned int timeGap = micros()-baseTime;
     
     //printf("\nLEFT: %.2f", Lpower);
@@ -39,8 +43,8 @@ void rwmReaderLFall(){
     //cout << "\r" << Lpower;
     Lpower = superMap(timeGap, 15, 2041, -1, 1);
 
-    baseTime = micros();
-    countoo++;
+    }
+    lHigh = !lHigh;
 }
 
 void counter(){
@@ -52,8 +56,9 @@ int main()
     wiringPiSetup();
     pinMode(LEFTPIN, INPUT);
     pinMode(RIGHTPIN, INPUT);
+    while(digitalRead(LEFTPIN) == 0){}
+    wiringPiISR (LEFTPIN, INT_EDGE_BOTH, &rwmReaderL;
 
-    wiringPiISR (LEFTPIN, INT_EDGE_BOTH, &rwmReaderLFall);
     //wiringPiISR (LEFTPIN, INT_EDGE_RISING, &rwmReaderLRise);
     //wiringPiISR (LEFTPIN, INT_EDGE_RISING, &counter);
     

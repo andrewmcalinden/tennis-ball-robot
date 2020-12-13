9396c4d1a7ef3039c39e700ce247ed9436bf05e4
Acloran <32380754+Acloran@users.noreply.github.com>
2020-12-12 20:44:42 -0600

    tuning still

--- General-Testing/movement/arduinoAsARemote.cpp
+++ General-Testing/movement/arduinoAsARemote.cpp
@@ -32,7 +32,7 @@ void rwmReaderL()
     {
         unsigned int timeGap = micros()-baseTimeL;
         //cout << timeGap << "\n";
-        Lpower = superMap(timeGap, 95, 1950, 1, -1);
+        Lpower = superMap(timeGap, 95, 1940, 1, -1);
         
     }
 

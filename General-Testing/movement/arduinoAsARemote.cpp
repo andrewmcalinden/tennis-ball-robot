60bbd11f52d2c6a28aeb3a33543eb04e318f9697
Acloran <andrew@cloran.com>
2020-12-22 15:43:09 -0600

    added back ball counter

--- General-Testing/movement/arduinoAsARemote.cpp
+++ General-Testing/movement/arduinoAsARemote.cpp
@@ -114,18 +114,18 @@ int main()
 
     wiringPiISR (LEFT_INPUT_PIN, INT_EDGE_BOTH, &rwmReaderL);
     wiringPiISR (RIGHT_INPUT_PIN, INT_EDGE_BOTH, &rwmReaderR);
-    //wiringPiISR (JOYSWITCH_INPUT_PIN, INT_EDGE_BOTH, &collectorToggle);
-    //wiringPiISR (COUNT_INPUT_PIN, INT_EDGE_FALLING, &countBalls);
+    wiringPiISR (JOYSWITCH_INPUT_PIN, INT_EDGE_BOTH, &collectorToggle);
+    wiringPiISR (COUNT_INPUT_PIN, INT_EDGE_FALLING, &countBalls);
 
     while (true)
     {
         motorL.setPower(lPower);
         motorR.setPower(rPower);
         
-        /*if (count > oldCount){
+        if (count > oldCount){
             printf("\r",count);
             oldCount = count;
-            }*/
+            }
     
     }
 }

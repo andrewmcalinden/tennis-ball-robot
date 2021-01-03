64f26c67520c4fe750bf932170eda807b5cd231f
Acloran <andrew@cloran.com>
2021-01-03 17:44:29 -0600

    less precise time

--- General-Testing/movement/robot.cpp
+++ General-Testing/movement/robot.cpp
@@ -151,7 +151,7 @@ void Robot::turnHeading(double finalAngle, double kp, double ki, double kd, doub
     double integral = 0;
 
     //for some reason, error threshold is 1 greater than what is typed in
-    while (timeAtSetPoint < .1)
+    while (timeAtSetPoint < .05)
     {
         updatePos(encoderL.read(), encoderR.read());
         error = angleDiff(globalHeading, finalAngle);

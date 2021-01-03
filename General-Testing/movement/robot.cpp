184460c5be95ec7222b058693f5911dd2f774136
Acloran <andrew@cloran.com>
2021-01-03 17:14:44 -0600

    print dt better

--- General-Testing/movement/robot.cpp
+++ General-Testing/movement/robot.cpp
@@ -158,7 +158,7 @@ void Robot::turnHeading(double finalAngle, double kp, double ki, double kd, doub
 
         currentTime = ((std::clock() - timer) / (double)CLOCKS_PER_SEC);
         const double dt = currentTime - pastTime;
-        printf("\tdt: %.2f", dt);
+        std::cout << "dt: " << std::scientific << dt; 
 
         //we negate error because 
         const double proportional = error / abs(initialAngleDiff);

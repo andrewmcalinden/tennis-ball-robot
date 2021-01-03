6caeaa7bceb31fec4ad1fdd7fa6a6ff0120dc3ad
Acloran <andrew@cloran.com>
2021-01-02 21:52:16 -0600

    print position

--- General-Testing/odometry/odomTest.cpp
+++ General-Testing/odometry/odomTest.cpp
@@ -8,10 +8,10 @@ int main()
     Encoder encoderR(3, 2);
     while (true)
     {
-        std::cout << "\rleft: " << encoderL.read() << "\tright: " << encoderR.read();
+        //std::cout << "\rleft: " << encoderL.read() << "\tright: " << encoderR.read();
         updatePos(encoderL.read(), encoderR.read());
-        // printf("\rX: %.2f", globalXPos);
-        // printf("   Y: %.2f", globalYPos);
-        // printf("   Heading: %.2f", globalHeading);
+        printf("\rX: %.2f", globalXPos);
+        printf("   Y: %.2f", globalYPos);
+        printf("   Heading: %.2f", globalHeading);
     }
 }

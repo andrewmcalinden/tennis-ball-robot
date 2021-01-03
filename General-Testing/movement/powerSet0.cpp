e4bdb7a8f87bdcfcd9214481ae257a85b3f07cc9
Acloran <andrew@cloran.com>
2021-01-03 14:46:02 -0600

    can motors stop?

---
+++ General-Testing/movement/powerSet0.cpp
@@ -0,0 +1,24 @@
+#include "motor.h"
+#include <iostream>
+#include <string>
+#include <wiringPi.h>
+#include<unistd.h>
+
+#define DIRECTION_PIN_FORWARD_LEFT 22
+#define POWER_PIN_LEFT 23
+#define DIRECTION_PIN_FORWARD_RIGHT 27
+#define POWER_PIN_RIGHT 26
+
+
+int main()
+{
+    Motor motorL(DIRECTION_PIN_FORWARD_LEFT, POWER_PIN_LEFT);
+    Motor motorR(DIRECTION_PIN_FORWARD_RIGHT, POWER_PIN_RIGHT);
+
+    motorL.setPower(.065);
+    motorR.setPower(-.065);
+    delay(5000);
+    std::cout << "stopping motors" << endl;
+    motorL.setPower(0);
+    motorR.setPower(0);
+}

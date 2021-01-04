8e99218acfabe86b21e437440be208d1f53e8345
Acloran <andrew@cloran.com>
2021-01-04 15:41:46 -0600

    add i

--- General-Testing/movement/movementTest.cpp
+++ General-Testing/movement/movementTest.cpp
@@ -18,10 +18,11 @@
 int main()
 {
     Robot r(LEFT_MOTOR_DIR_PIN, LEFT_MOTOR_POWER_PIN, RIGHT_MOTOR_DIR_PIN, RIGHT_MOTOR_POWER_PIN, INITIAL_X, INITIAL_Y, INITIAL_THETA, L_ENCODER_PIN1, L_ENCODER_PIN2, R_ENCODER_PIN1, R_ENCODER_PIN2);
-    //r.turnHeading(90, .37, 0, .00004, .08); //WORKS GOOD, NO OVERSHOOT
-    r.turnHeading(90, .135, 0, 0, .08);
+    //r.turnHeading(90, .37, 0, .00004, .08); //WORK IN PROGRESS
+    //r.turnHeading(90, .135, 0, 0, .08); //SLOW AND DOESNT OVERSHOOT 
     // r.turnHeading(-30, .17, 0, .0001, .04);
     // r.turnHeading(90, .17, 0, .0001, .04);
     // r.turnHeading(0, .17, 0, .0001, .04);
     //r.goStraight(20, .4, 0, 0, .04);
+    r.turnHeading(90, .37, .0001, 0, .08);
 }

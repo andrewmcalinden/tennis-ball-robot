63ca7d0eadfc3c7b7e7be8e2cddd09e9da3798d8
Acloran <andrew@cloran.com>
2021-01-08 20:27:14 -0600

    Revert "delay"
    This reverts commit 1b7da4b80238df6da10071bf46e4a67e645aac0c.

--- General-Testing/movement/robot.cpp
+++ General-Testing/movement/robot.cpp
@@ -239,7 +239,7 @@ void Robot::turnHeading(double finalAngle, double kp, double ki, double kd, doub
 
         pastTime = currentTime;
         pastError = error;
-        delay(25);
+        millis(25);
     }
     std::cout << "\nWE ARE STOPPING MOTORS!!!!!!!!!!!!!!!!!!" << std::endl << "abs error: " << fabs(error) << std::endl;
     setMotorPowers(0, 0);

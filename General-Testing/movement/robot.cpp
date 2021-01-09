a1a5111adb6149ee59febc588d0cea7654f35a52
Acloran <andrew@cloran.com>
2021-01-08 20:49:52 -0600

    Update robot.cpp

--- General-Testing/movement/robot.cpp
+++ General-Testing/movement/robot.cpp
@@ -238,7 +238,7 @@ void Robot::turnHeading(double finalAngle, double kp, double ki, double kd, doub
 
         pastTime = currentTime;
         pastError = error;
-        delay(30);
+        delay(25);
     }
     std::cout << "\nWE ARE STOPPING MOTORS!!!!!!!!!!!!!!!!!!" << std::endl << "abs error: " << fabs(error) << std::endl;
     setMotorPowers(0, 0);

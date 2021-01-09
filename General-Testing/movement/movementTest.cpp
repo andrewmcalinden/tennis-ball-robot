5a26a78347406590c3ead11bcea31ba230195bd9
Acloran <andrew@cloran.com>
2021-01-08 21:59:06 -0600

    square

--- General-Testing/movement/movementTest.cpp
+++ General-Testing/movement/movementTest.cpp
@@ -18,16 +18,16 @@
 int main()
 {
     Robot r(LEFT_MOTOR_DIR_PIN, LEFT_MOTOR_POWER_PIN, RIGHT_MOTOR_DIR_PIN, RIGHT_MOTOR_POWER_PIN, INITIAL_X, INITIAL_Y, INITIAL_THETA, L_ENCODER_PIN1, L_ENCODER_PIN2, R_ENCODER_PIN1, R_ENCODER_PIN2);
-    // int i = 4;
-    // int start = 90;
-    // while (i--)
-    // {
-    //     r.goStraight(40, .4, 0, .00004, .08);
-    //     r.turnHeading(start, .37, 0, .00002, .08);
-    //     start += 90;
-    // }
+    int i = 4;
+    int start = 90;
+    while (i--)
+    {
+        r.goStraight(40, .4, 0, .00004, .12);
+        r.turnHeading(start, .37, .00003, .00004, .12);
+        start += 90;
+    }
 
-    r.turnHeading(90, .37, .00003, .00004, .12);
+    //r.turnHeading(90, .37, .00003, .00004, .12);
 
     
 

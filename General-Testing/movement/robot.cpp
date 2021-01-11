e1a2bf5448974305ca925e87fea9d6beb8931c92
Acloran <andrew@cloran.com>
2021-01-11 16:56:06 -0600

    5 delay

--- General-Testing/movement/robot.cpp
+++ General-Testing/movement/robot.cpp
@@ -202,7 +202,7 @@ void Robot::turnHeading(double finalAngle, double kp, double ki, double kd, doub
 
         pastTime = currentTime;
         pastError = error;
-        delay(1);
+        delay(5);
     }
     std::cout << "abs error: " << fabs(error) << std::endl;
     setMotorPowers(0, 0);

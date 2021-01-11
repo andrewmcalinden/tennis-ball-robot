c887eecc898ed57e22278c73e99a441c1877a9b5
Acloran <andrew@cloran.com>
2021-01-11 17:17:44 -0600

    working time

--- General-Testing/misc/clockTest.cpp
+++ General-Testing/misc/clockTest.cpp
@@ -6,15 +6,17 @@ int main()
     std::clock_t timer;
     timer = std::clock();
 
-    const unsigned int delayAmount = 50;
+    const unsigned int delayAmount = 1000;
 
     double time = 0;
+    int numDelays = 0;
 
     while(true)
     {
-        time += ((std::clock() - timer) / (double)CLOCKS_PER_SEC) + (delayAmount / 1000.0);
+        time = ((std::clock() - timer) / (double)CLOCKS_PER_SEC) + ((delayAmount / 1000.0) * numDelays);
         //time += (delayAmount / 1000.0);
         std::cout << time << "\n";
         delay(delayAmount);
+        ++numDelays;
     }
 }

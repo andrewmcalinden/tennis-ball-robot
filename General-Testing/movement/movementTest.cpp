9fbf3ff31bbd0eae7ae496a24490b2ba037521f1
Acloran <andrew@cloran.com>
2021-01-26 16:52:22 -0600

    less max speed

--- General-Testing/movement/movementTest.cpp
+++ General-Testing/movement/movementTest.cpp
@@ -20,19 +20,19 @@ int main()
 {
     Robot r(LEFT_MOTOR_DIR_PIN, LEFT_MOTOR_POWER_PIN, RIGHT_MOTOR_DIR_PIN, RIGHT_MOTOR_POWER_PIN, INITIAL_X, INITIAL_Y, INITIAL_THETA, L_ENCODER_PIN1, L_ENCODER_PIN2, R_ENCODER_PIN1, R_ENCODER_PIN2);
     r.goToPos(0, 60,
-              .37, 0, .002, .12, .7,
+              .37, 0, .002, .12, .5,
               .37, 0, .0006, .12);
 
     r.goToPos(-60, 60,
-              .37, 0, .002, .12, .7,
+              .37, 0, .002, .12, .5,
               .37, 0, .0006, .12);
 
     r.goToPos(-60, 0,
-              .37, 0, .002, .12, .7,
+              .37, 0, .002, .12, .5,
               .37, 0, .0006, .12);
 
     r.goToPos(0, 0,
-              .37, 0, .002, .12, .7,
+              .37, 0, .002, .12, .5,
               .37, 0, .0006, .12);
 
     //BEST CONSTANTS SO FAR WITH NEW METHOD

bb3b5614bb54e247c81a07a7ac181cbd66c49827
Acloran <32380754+Acloran@users.noreply.github.com>
2021-01-31 16:06:51 -0600

    changing led

--- python/xboxDrive/RocketLeagueDrive.py
+++ python/xboxDrive/RocketLeagueDrive.py
@@ -1,22 +1,18 @@
 from __future__ import print_function
-
+import RPi.GPIO as GPIO
 import xbox
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.OUT)
+
+
+
 
 # Format floating point number to string format -x.xxx
 def fmtFloat(n):
     return '{:6.3f}'.format(n)
 
-# Print one or more values without a line feed
-def show(*args):
-    for arg in args:
-        print(arg, end="")
-
-# Print true or false value based on a boolean, without linefeed
-def showIf(boolean, ifTrue, ifFalse=" "):
-    if boolean:
-        show(ifTrue)
-    else:
-        show(ifFalse)
 
 # Instantiate the controller
 joy = xbox.Joystick()
@@ -25,17 +21,20 @@ joy = xbox.Joystick()
 print("Xbox controller sample: Press Back button to exit")
 while not joy.Back():
     # Show connection status
-    show("Connected:")
-    showIf(joy.connected(), "Y", "N")
+    
+    #showIf(joy.connected(), "Y", "N")
     # Left analog stick
-    show("  Left X/Y:", fmtFloat(joy.leftX()), "/", fmtFloat(joy.leftY()))
+    #show("  Left X/Y:", fmtFloat(joy.leftX()), "/", fmtFloat(joy.leftY()))
     # Right trigger
     
-    show("  RightTrg:", fmtFloat(joy.rightTrigger()))
+    #show("  RightTrg:", fmtFloat(joy.rightTrigger()))
     # A/B/X/Y buttons
-    show("  LeftTrg:", fmtFloat(joy.leftTrigger()))
-    show("  Buttons:")
-    showIf(joy.A(), "A")
-    show(chr(13))
+    #show("  LeftTrg:", fmtFloat(joy.leftTrigger()))
+    #show("  Buttons:")
+    if joy.A():
+        GPIO.output(17, 1)
+    else:
+        GPIO.output(17, 0)
+    
 # Close out when done
 joy.close()

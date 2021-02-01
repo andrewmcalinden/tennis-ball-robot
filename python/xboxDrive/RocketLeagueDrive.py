86217274bf24dd23ea7c28e65b6d24f9fa097e10
Acloran <andrew@cloran.com>
2021-02-01 11:48:23 -0600

    now printing left stick x values

--- python/xboxDrive/RocketLeagueDrive.py
+++ python/xboxDrive/RocketLeagueDrive.py
@@ -83,14 +83,14 @@ while not joy.Back():
             collectOn = True
         
         while joy.A():
-            time.sleep(.001)
+            time.sleep(.01)
 
     #GPIO.add_event_detect(countPin, GPIO.RISING)
 
     #if GPIO.event_detected(countPin):
         #ballCount+=1
 
-    print("right: " + fmtFloat(rightPower))
+    print("right: " + fmtFloat(joy.leftX()))
 
 
     # Show connection status

eb45ecf7afb716ea128601cc25a9ebda8b62558f
Acloran <32380754+Acloran@users.noreply.github.com>
2021-02-19 15:02:43 -0600

    tune attempt

--- general_testing/vision/houghDetect.cpp
+++ general_testing/vision/houghDetect.cpp
@@ -17,9 +17,9 @@ int main(int argc, char** argv)
     cvtColor(src, gray, COLOR_BGR2GRAY);
     medianBlur(gray, gray, 7);
     vector<Vec3f> circles;
-    HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
+    HoughCircles(gray, circles, HOUGH_GRADIENT, .9,
                  gray.rows/64,  // change this value to detect circles with different distances to each other
-                 80, 30, 20, 50 // change the last two parameters
+                 120, 30, 20, 50 // change the last two parameters
             // (min_radius & max_radius) to detect larger circles
     );
     for( size_t i = 0; i < circles.size(); i++ )

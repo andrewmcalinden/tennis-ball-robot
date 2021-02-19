c14dc00dc58cacaaef65df36e6b6952a782b9a41
Acloran <32380754+Acloran@users.noreply.github.com>
2021-02-19 15:05:31 -0600

    reduce min radius

--- general_testing/vision/houghDetect.cpp
+++ general_testing/vision/houghDetect.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
     vector<Vec3f> circles;
     HoughCircles(gray, circles, HOUGH_GRADIENT, .8,
                  gray.rows/64,  // change this value to detect circles with different distances to each other
-                 130, 30, 20, 50 // change the last two parameters
+                 130, 30, 5, 50 // change the last two parameters
             // (min_radius & max_radius) to detect larger circles
     );
     for( size_t i = 0; i < circles.size(); i++ )

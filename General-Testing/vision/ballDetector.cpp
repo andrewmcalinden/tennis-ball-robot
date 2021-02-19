a0d44f3bdfab783d79c5b1d6ca2349f08faa7c1e
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2021-02-18 20:35:11 -0600

    vision

---
+++ General-Testing/vision/ballDetector.cpp
@@ -0,0 +1,53 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <sstream>
+
+using namespace cv;
+using namespace std;
+
+int hmin = 37, smin = 106, vmin = 123;
+int hmax = 53, smax = 208, vmax = 255;
+
+void main()
+{
+    Mat img = imread("Images/view1.jpg");
+    Mat imgHSV, imgMask, imgResize, imgDilate;
+
+    resize(img, imgResize, Size(), .25, .25);
+
+    cvtColor(imgResize, imgHSV, COLOR_BGR2HSV);
+
+    Scalar lower(hmin, smin, vmin);
+    Scalar upper(hmax, smax, vmax);
+
+    inRange(imgHSV, lower, upper, imgMask);
+
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+    dilate(imgMask, imgDilate, kernel);
+
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(imgDilate, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    drawContours(imgResize, contours, -1, Scalar(255, 0, 255), 1);
+
+    for (vector<Point> &contour : contours)
+    {
+        Moments m = moments(contour);
+        double xCenter = m.m10 / m.m00;
+        double yCenter = m.m01 / m.m00;
+
+        stringstream point;
+        point << "(" << xCenter << ", " << yCenter << ")";
+        string text = point.str();
+
+        putText(imgResize, text, Point((int)xCenter, (int)yCenter), FONT_HERSHEY_DUPLEX, .3, Scalar(255, 255, 255), .2);
+        cout << xCenter << ", " << yCenter << endl;
+    }
+
+    imshow("mask", imgMask);
+    imshow("dilated masked", imgDilate);
+    imshow("original", imgResize);
+    waitKey(0);
+}
---
+++ General-Testing/vision/cameraTest.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <raspicam/raspicam_cv.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    raspicam::RaspiCam_Cv cap;
+    Mat img;
+
+    if (!cap.isOpened())
+    {
+        cout << "Could not initialize capturing..." << endl;
+        return 0;
+    }
+
+    while (1)
+    {
+        cap.grab();
+        cap.retrieve(img);
+        imshow("image", img);
+        waitKey(1);
+    }
+}

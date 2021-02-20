9268c6a9f63d8fe2829db0a22f251a2462089fe4
Acloran <32380754+Acloran@users.noreply.github.com>
2021-02-20 12:47:02 -0600

    less sensitive

--- general_testing/vision/houghDetect.cpp
+++ general_testing/vision/houghDetect.cpp
@@ -31,7 +31,7 @@ int main(int argc, char** argv)
     imshow("img view", gray);
     HoughCircles(gray, circles, HOUGH_GRADIENT, .8,
                  gray.rows/32,  // change this value to detect circles with different distances to each other
-                 60, 30, 10, 40 // change the last two parameters
+                 60, 25, 5, 40 // change the last two parameters
             // (min_radius & max_radius) to detect larger circles
     );
     for( size_t i = 0; i < circles.size(); i++ )

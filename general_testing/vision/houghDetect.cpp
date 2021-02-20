6b77e39fab8f28f0c0a9e1eafc5f56d8457beb10
Acloran <32380754+Acloran@users.noreply.github.com>
2021-02-20 15:06:28 -0600

    canny

--- general_testing/vision/houghDetect.cpp
+++ general_testing/vision/houghDetect.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
     blur(gray, gray, Size(5,5));
 
     
-    Canny(gray, gray, 30, 90, 5);
+    Canny(gray, gray, 30, 90, 3);
     Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5));
     
     dilate(gray, gray, kernel);

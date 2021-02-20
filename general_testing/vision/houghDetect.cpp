05c5ac7e4fbbccbea00f63df5d62d13356c2f2b2
Acloran <32380754+Acloran@users.noreply.github.com>
2021-02-20 15:13:22 -0600

    backing off parem 2

--- general_testing/vision/houghDetect.cpp
+++ general_testing/vision/houghDetect.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
     imshow("img view", gray);
     HoughCircles(gray, circles, HOUGH_GRADIENT, .75,
                  gray.rows/20,  // change this value to detect circles with different distances to each other
-                 110, 30, 3, 40 // change the last two parameters
+                 110, 25, 3, 40 // change the last two parameters
             // (min_radius & max_radius) to detect larger circles
     );
     for( size_t i = 0; i < circles.size(); i++ )

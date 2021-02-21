083983d930019b33b4951c3495051939e8797679
Acloran <32380754+Acloran@users.noreply.github.com>
2021-02-21 15:47:18 -0600

    90fps

--- general_testing/vision/new_images/takeAPic.cpp
+++ general_testing/vision/new_images/takeAPic.cpp
@@ -11,14 +11,14 @@ int main(int argc, char** argv)
     VideoCapture cap(0);
 
 
-    int height(720);
-    int width(1280);
+    int height(480);
+    int width(640);
 
     
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, height);
     cap.set(cv::CAP_PROP_FRAME_WIDTH, width);
     //cap.set(cv::CAP_PROP_FOURCC, 0x21);
-    cap.set(cv::CAP_PROP_FPS, 60);
+    cap.set(cv::CAP_PROP_FPS, 90);
 
     int count = 1;
     std::stringstream ss;

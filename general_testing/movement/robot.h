126955ede95bf3f994280f2b39bac56ed00a3d2e
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2021-03-20 16:58:17 -0500

    errors

--- general_testing/movement/robot.h
+++ general_testing/movement/robot.h
@@ -17,20 +17,21 @@ class Robot
     unsigned char ballCounterPin;
 
     public:
-    Robot(unsigned char lMotorDirPin, unsigned char lMotorPowerPin, unsigned char rMotorDirPin, unsigned char rMotorPowerPin,
-            double initialX, double initialY, double initialTheta,
-            unsigned char lEncoderPin1, unsigned char lEncoderPin2, unsigned char rEncoderPin1, unsigned char rEncoderPin2);
+        Robot(unsigned char lMotorDirPin, unsigned char lMotorPowerPin, unsigned char rMotorDirPin, unsigned char rMotorPowerPin,
+              double initialX, double initialY, double initialTheta,
+              unsigned char lEncoderPin1, unsigned char lEncoderPin2, unsigned char rEncoderPin1, unsigned char rEncoderPin2,
+              unsigned char counterPin);
 
-    void goStraight(double inches, double kp, double ki, double kd, double f, double maxSpeed);
-    void turnHeading(double finalAngle, double kp, double ki, double kd, double f); //in degrees, turns to that heading (field centric)
-    void goToPos(double x, double y,
-                 double kp_straight, double ki_straight, double kd_straight, double f_straight, double maxSpeed_straight,
-                 double kp_turn, double ki_turn, double kd_turn, double f_turn);
+        void goStraight(double inches, double kp, double ki, double kd, double f, double maxSpeed);
+        void turnHeading(double finalAngle, double kp, double ki, double kd, double f); //in degrees, turns to that heading (field centric)
+        void goToPos(double x, double y,
+                     double kp_straight, double ki_straight, double kd_straight, double f_straight, double maxSpeed_straight,
+                     double kp_turn, double ki_turn, double kd_turn, double f_turn);
 
-    void setMotorPowers(double lPower, double rPower);
+        void setMotorPowers(double lPower, double rPower);
 
-    void turnPixel(double finalPixel, double power, double f, cv::Rect2d initialBB);
-    void curveToBall(cv::Rect2d initialBB, double power, double f);
-    void goToBall();
+        void turnPixel(double finalPixel, double power, double f, cv::Rect2d initialBB);
+        void curveToBall(cv::Rect2d initialBB, double power, double f);
+        void goToBall();
 };
 #endif

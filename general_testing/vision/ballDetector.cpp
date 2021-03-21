f8f1ddcb86951257afb3568181ee47c05a3d52fb
Acloran <andrew@cloran.com>
2021-03-20 20:06:26 -0500

    mil tracker

--- general_testing/vision/ballDetector.cpp
+++ general_testing/vision/ballDetector.cpp
@@ -80,7 +80,7 @@ void trackBall(Rect2d initialBBox)
         startCamera();
     }
 
-    Ptr<Tracker> tracker = TrackerCSRT::create();
+    Ptr<Tracker> tracker = TrackerMIL::create();
     Mat frame;
     cap.grab();
     cap.retrieve(frame);

86ea43bbceb1867c9ab73c2421995098216a4ea7
Acloran <andrew@cloran.com>
2021-03-22 17:00:44 -0500

    court clearing sim

--- general_testing/movement/movementTest.cpp
+++ general_testing/movement/movementTest.cpp
@@ -22,10 +22,21 @@
 int main()
 {
     Robot r(LEFT_MOTOR_DIR_PIN, LEFT_MOTOR_POWER_PIN, RIGHT_MOTOR_DIR_PIN, RIGHT_MOTOR_POWER_PIN, INITIAL_X, INITIAL_Y, INITIAL_THETA, L_ENCODER_PIN1, L_ENCODER_PIN2, R_ENCODER_PIN1, R_ENCODER_PIN2, COUNT_INPUT_PIN);
-    r.goToPos(8 , 24,
-              .55, 0, .01, .1, .5,
+    r.goToPos(0 , 120,
+              .55, 0, .01, .11, .5,
               .5, 0, .002, .12);
-    r.turnHeading(0, .5, 0, .002, .12);
+    r.goToPos(36, 120,
+              .55, 0, .01, .11, .5,
+              .5, 0, .002, .12);
+    r.goToPos(36, 0,
+              .55, 0, .01, .11, .5,
+              .5, 0, .002, .12);
+    r.goToPos(72, 0,
+              .55, 0, .01, .11, .5,
+              .5, 0, .002, .12);
+    r.goToPos(72, 120,
+              .55, 0, .01, .11, .5,
+              .5, 0, .002, .12);
     // r.goToPos(80, 160,
     //           .37, 0, .002, .12, .5,
     //           .37, 0, .0006, .12);
@@ -50,6 +61,9 @@ int main()
 
     // r.turnHeading(90, .37, 0, .0006, .12);
     //BEST CONSTANTS SO FAR WITH NEW METHOD
+    r.goToPos(8, 24,
+              .55, 0, .01, .11, .5,
+              .5, 0, .002, .12);
     //r.turnHeading(90, .37, 0, .0006, .12);
     //r.goStraight(-75, .37, 0, .002, .12, .7);
 }

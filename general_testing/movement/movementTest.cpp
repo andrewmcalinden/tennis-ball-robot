8d8b64d0316fbba166dccef3f424de8a591f4ad5
Acloran <andrew@cloran.com>
2021-03-22 16:57:49 -0500

    Update movementTest.cpp

--- general_testing/movement/movementTest.cpp
+++ general_testing/movement/movementTest.cpp
@@ -23,9 +23,9 @@ int main()
 {
     Robot r(LEFT_MOTOR_DIR_PIN, LEFT_MOTOR_POWER_PIN, RIGHT_MOTOR_DIR_PIN, RIGHT_MOTOR_POWER_PIN, INITIAL_X, INITIAL_Y, INITIAL_THETA, L_ENCODER_PIN1, L_ENCODER_PIN2, R_ENCODER_PIN1, R_ENCODER_PIN2, COUNT_INPUT_PIN);
     r.goToPos(8 , 24,
-              .55, 0, .008, .1, .5,
+              .55, 0, .01, .1, .5,
               .5, 0, .002, .12);
-    r.turnHeading(90, .5, 0, .0002, .12);
+    r.turnHeading(0, .5, 0, .002, .12);
     // r.goToPos(80, 160,
     //           .37, 0, .002, .12, .5,
     //           .37, 0, .0006, .12);

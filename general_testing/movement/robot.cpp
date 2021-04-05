b36be913f51eec9ead29e6ea269f185d7c04ce16
Acloran <andrew@cloran.com>
2021-04-05 15:29:47 -0500

    just right

--- general_testing/movement/robot.cpp
+++ general_testing/movement/robot.cpp
@@ -426,7 +426,7 @@ void Robot::curveToBall(cv::Rect2d initialBB, double power, double f)
 
         // setMotorPowers(lPower + f, rPower + f);
         //std::cout << "count: " << ballCount << std::endl;
-        setMotorPowers(1, 1);
+        setMotorPowers(0, 1);
         delay(2000);
     //}
 

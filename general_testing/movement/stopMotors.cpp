78551fe7520a9ff18e46a2acd5651e02b971c69c
Acloran <andrew@cloran.com>
2021-04-09 20:50:43 -0500

    stop collector in stop motors

--- general_testing/movement/stopMotors.cpp
+++ general_testing/movement/stopMotors.cpp
@@ -2,6 +2,7 @@
 #define POWER_PIN_LEFT 23
 #define DIRECTION_PIN_FORWARD_RIGHT 27
 #define POWER_PIN_RIGHT 26
+#define COLLECTOR_PIN 4
 
 #include <iostream>
 #include "../motor/motor.h"
@@ -11,7 +12,9 @@ int main()
 {
     Motor motorL(DIRECTION_PIN_FORWARD_LEFT, POWER_PIN_LEFT);
     Motor motorR(DIRECTION_PIN_FORWARD_RIGHT, POWER_PIN_RIGHT);
+    pinMode(COLLECTOR_PIN, OUTPUT);
 
     motorL.setPower(0);
     motorR.setPower(0);
+    digitalWrite(COLLECTOR_PIN, LOW);
 }

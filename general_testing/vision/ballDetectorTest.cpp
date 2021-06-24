c293ecaf396ae59d680ed869248bc5abc5ffd91d
Andrew McAlinden <52133386+droiddoes9@users.noreply.github.com>
2021-06-23 21:11:07 -0500

    use test image, dont resize

--- general_testing/vision/ballDetectorTest.cpp
+++ general_testing/vision/ballDetectorTest.cpp
@@ -7,47 +7,46 @@
 using namespace cv;
 using namespace std;
 //day values
-// int hmin = 37, smin = 106, vmin = 123;
-// int hmax = 53, smax = 208, vmax = 255;
+int hmin = 37, smin = 106, vmin = 123;
+int hmax = 53, smax = 208, vmax = 255;
 
 //burning night light values
-int hmin = 38, smin = 0, vmin = 0;
-int hmax = 82, smax = 208, vmax = 255;
+// int hmin = 38, smin = 0, vmin = 0;
+// int hmax = 82, smax = 208, vmax = 255;
 
 int main()
 {
-    VideoCapture cap(0);
+    //VideoCapture cap(0);
     Mat img, imgHSV, imgBlur, imgMask, imgResize, imgDilate;
 
-    if (!cap.isOpened())
-    {
-        cout << "Could not initialize capturing..." << endl;
-        return 0;
-    }
+    // if (!cap.isOpened())
+    // {
+    //     cout << "Could not initialize capturing..." << endl;
+    //     return 0;
+    // }
     
     
     namedWindow("Trackbars", (640, 200));
-    createTrackbar("hue min", "Trackbars", &hmin, 500);
-    createTrackbar("hue max", "Trackbars", &hmax, 500);
-    createTrackbar("sat min", "Trackbars", &smin, 500);
-    createTrackbar("sat max", "Trackbars", &smax, 500);
-    createTrackbar("val min", "Trackbars", &vmin, 500);
-    createTrackbar("val max", "Trackbars", &vmax, 500);
-    
+    createTrackbar("hue min", "Trackbars", &hmin, 179);
+    createTrackbar("hue max", "Trackbars", &hmax, 179);
+    createTrackbar("sat min", "Trackbars", &smin, 255);
+    createTrackbar("sat max", "Trackbars", &smax, 255);
+    createTrackbar("val min", "Trackbars", &vmin, 255);
+    createTrackbar("val max", "Trackbars", &vmax, 255);
 
     while(1)
     {
-        cap.grab();
-        cap.retrieve(img);
+        // cap.grab();
+        // cap.retrieve(img);
         // stringstream path;
         // path << "new_images/image_" << i << ".png";
-        // img = imread(path.str());
+        img = imread("image_1.png");
 
-        resize(img, imgResize, Size(), .75, .75);
-        Size s = imgResize.size();
-        cout << s.width << " " << s.height << endl;
+        // resize(img, imgResize, Size(), .75, .75);
+        // Size s = imgResize.size();
+       // cout << s.width << " " << s.height << endl;
 
-        cvtColor(imgResize, imgHSV, COLOR_BGR2HSV);
+        cvtColor(img, imgHSV, COLOR_BGR2HSV);
        // GaussianBlur(imgHSV, imgBlur, Size(11, 11), 0);
 
         Scalar lower(hmin, smin, vmin);
@@ -60,7 +59,7 @@ int main()
         vector<vector<Point>> contours;
         vector<Vec4i> hierarchy;
         findContours(imgDilate, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-        drawContours(imgResize, contours, -1, Scalar(255, 0, 255), 1);
+        drawContours(img, contours, -1, Scalar(255, 0, 255), 1);
 
         for (vector<Point> &contour : contours)
         {
@@ -72,17 +71,17 @@ int main()
             point << "(" << xCenter << ", " << yCenter << ")";
             string text = point.str();
 
-            putText(imgResize, text, Point((int)xCenter, (int)yCenter), FONT_HERSHEY_DUPLEX, .3, Scalar(255, 255, 255), .2);
+            putText(img, text, Point((int)xCenter, (int)yCenter), FONT_HERSHEY_DUPLEX, .3, Scalar(255, 255, 255), .2);
             cout << xCenter << ", " << yCenter << endl;
         }
 
-        line(imgResize, Point(62, 707), Point(716, 219), Scalar(0, 0, 255), 2);
-        line(imgResize, Point(1218, 707), Point(564, 219), Scalar(0, 0, 255), 2);
+        // line(imgResize, Point(62, 707), Point(716, 219), Scalar(0, 0, 255), 2);
+        // line(imgResize, Point(1218, 707), Point(564, 219), Scalar(0, 0, 255), 2);
 
 
         imshow("mask", imgMask);
         imshow("dilated masked", imgDilate);
-        imshow("original", imgResize);
+        imshow("original", img);
         // char c=(char)waitKey(1);
         // while(c!=32){ c=(char)waitKey(1);} // do nothing
         waitKey(1);

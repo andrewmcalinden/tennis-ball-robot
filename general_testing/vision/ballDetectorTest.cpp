a1d1477d75902922a56ab698c253a9365d6f963c
Acloran <andrew@cloran.com>
2021-07-22 19:39:05 -0500

    that didnt work

--- general_testing/vision/ballDetectorTest.cpp
+++ general_testing/vision/ballDetectorTest.cpp
@@ -1,7 +1,7 @@
-// #include <opencv2/imgcodecs.hpp>
-// #include <opencv2/highgui.hpp>
-// #include <opencv2/imgproc.hpp>
-#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+//#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <sstream>
 

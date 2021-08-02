5f8ada8eb419f24ea191304c3665bba67b76ec86
Andrew McAlinden <52133386+droiddoes9@users.noreply.github.com>
2021-08-01 21:26:51 -0500

    simple test

--- general_testing/vision/cameraTest.cpp
+++ general_testing/vision/cameraTest.cpp
@@ -8,22 +8,26 @@ using namespace std;
 
 int main()
 {
-    VideoCapture cap(0);
-    Mat img;
+    // VideoCapture cap(0);
+    // Mat img;
 
-    cap.set(CAP_PROP_BUFFERSIZE, 1);
+    // cap.set(CAP_PROP_BUFFERSIZE, 1);
 
-    if (!cap.isOpened())
-    {
-        cout << "Could not initialize capturing..." << endl;
-        return 0;
-    }
+    // if (!cap.isOpened())
+    // {
+    //     cout << "Could not initialize capturing..." << endl;
+    //     return 0;
+    // }
 
-    while (1)
-    {
-        cap.grab();
-        cap.retrieve(img);
-        imshow("image", img);
-        waitKey(0);
-    }
+    // while (1)
+    // {
+    //     cap.grab();
+    //     cap.retrieve(img);
+    //     imshow("image", img);
+    //     waitKey(0);
+    // }
+
+    cout << "beginning" << endl;
+    waitKey(0);
+    cout << "end" << endl;
 }

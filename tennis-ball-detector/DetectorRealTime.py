544ae54aa00ac01a384a9505596d898b0f825545
Acloran <32380754+Acloran@users.noreply.github.com>
2021-08-09 16:41:44 -0500

    reduced resolution

--- tennis-ball-detector/DetectorRealTime.py
+++ tennis-ball-detector/DetectorRealTime.py
@@ -14,8 +14,8 @@ classifier = cv2.CascadeClassifier(classifier)
 fpsLimit = 1 # throttle limit
 startTime = time.time()
 cap = cv2.VideoCapture(0)
-cap.set(4, 1080)
-cap.set(3, 1920)
+cap.set(3, 720)
+cap.set(4, 1280)
 
 
 while True:

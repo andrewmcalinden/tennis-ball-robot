bf9c19290859f444805837a97adb5e9d05e6895b
Acloran <32380754+Acloran@users.noreply.github.com>
2021-08-09 18:06:05 -0500

    added calibrated distance formula

--- tennis-ball-detector/DetectorRealTime.py
+++ tennis-ball-detector/DetectorRealTime.py
@@ -2,6 +2,7 @@ import cv2
 import sys
 import os
 import time
+import math
 
 # Run Command:
 #python3 DetectorRealTime.py data/cascade.xml
@@ -21,19 +22,19 @@ cap.set(3, 1280)
 while True:
     ret_val, img = cap.read()
 
-    nowTime = time.time()
-    frameRate = 30
-    frameRate = 1/(nowTime - startTime)
-    print("FPS:",frameRate, end="\r")
+    #nowTime = time.time()
+    #frameRate = 30
+    #frameRate = 1/(nowTime - startTime)
+    #print("FPS:",frameRate, end="\r")
     img_gray = cv2.cvtColor(cv2.UMat(img), cv2.COLOR_BGR2GRAY)
     found = classifier.detectMultiScale(img_gray, minSize =(10, 10))
     if(len(found)!=0):
         
             for i, (x, y, width, height) in enumerate(found):
                 cv2.rectangle(img, (x, y), (x + height, y + width), (0, 255, 0), 5)
-                print(height,"\r")
+                print((133.5879*math.e)^(-0.0129*height),"Inches",end="\r")
     cv2.imshow("output", img)
     if cv2.waitKey(1) == 27: 
         break  # esc to quit
-    startTime = time.time() # reset time
+    #startTime = time.time() # reset time
 cv2.destroyAllWindows()

7757d9261e67ca046debf8a936d0e9f145f6db63
Acloran <32380754+Acloran@users.noreply.github.com>
2021-08-09 16:44:47 -0500

    no limit

--- tennis-ball-detector/DetectorRealTime.py
+++ tennis-ball-detector/DetectorRealTime.py
@@ -12,26 +12,26 @@ classifier = cv2.CascadeClassifier(classifier)
 
 
 fpsLimit = 1 # throttle limit
-startTime = time.time()
+#startTime = time.time()
 cap = cv2.VideoCapture(0)
-cap.set(3, 720)
-cap.set(4, 1280)
+cap.set(3, 600)
+cap.set(4, 800)
 
 
 while True:
     ret_val, img = cap.read()
-    nowTime = time.time()
-    if (int(nowTime - startTime)) > fpsLimit:
+    #nowTime = time.time()
+    #if (int(nowTime - startTime)) > fpsLimit:
         
-        img_gray = cv2.cvtColor(cv2.UMat(img), cv2.COLOR_BGR2GRAY)
-        found = classifier.detectMultiScale(img_gray, minSize =(10, 10))
-        if(len(found)!=0):
-            #with open(f"results/{image[:-4]}_balls.txt", 'w') as f:
-                for i, (x, y, width, height) in enumerate(found):
-                    cv2.rectangle(img, (x, y), (x + height, y + width), (0, 255, 0), 5)
-                    #f.write(f"ball_no_{i}: coords ({x}, {y}), size {height}x{width}\n")
-        cv2.imshow("output", img)
-        if cv2.waitKey(1) == 27: 
-            break  # esc to quit
-        startTime = time.time() # reset time
+    img_gray = cv2.cvtColor(cv2.UMat(img), cv2.COLOR_BGR2GRAY)
+    found = classifier.detectMultiScale(img_gray, minSize =(10, 10))
+    if(len(found)!=0):
+        #with open(f"results/{image[:-4]}_balls.txt", 'w') as f:
+            for i, (x, y, width, height) in enumerate(found):
+                cv2.rectangle(img, (x, y), (x + height, y + width), (0, 255, 0), 5)
+                #f.write(f"ball_no_{i}: coords ({x}, {y}), size {height}x{width}\n")
+    cv2.imshow("output", img)
+    if cv2.waitKey(1) == 27: 
+        break  # esc to quit
+        #startTime = time.time() # reset time
 cv2.destroyAllWindows()

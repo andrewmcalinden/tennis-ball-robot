fb32670f90fdd0675242b9387f63c6f79b384e02
Acloran <32380754+Acloran@users.noreply.github.com>
2021-08-09 16:20:51 -0500

    debuig

--- tennis-ball-detector/camtest.py
+++ tennis-ball-detector/camtest.py
@@ -16,11 +16,14 @@ cap = cv2.VideoCapture(0)
 
 
 while True:
-    img = cap.read
-    nowTime = time.time()
-    if (int(nowTime - startTime)) > fpsLimit:
+    ret_val, img = cap.read()
+    #nowTime = time.time()
+    #if (int(nowTime - startTime)) > fpsLimit:
         
         #img_gray = cv2.cvtColor(cv2.UMat(img), cv2.COLOR_BGR2GRAY)
         
-        cv2.imshow("output",img)
-        startTime = time.time() # reset time
+    cv2.imshow("output",img)
+    if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+cv2.destroyAllWindows()
+        #startTime = time.time() # reset time

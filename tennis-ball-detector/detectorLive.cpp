ade385a60afe0f89954d45501cc0894dbe21c86d
Andrew McAlinden <52133386+droiddoes9@users.noreply.github.com>
2021-08-14 22:44:55 -0500

    live testing

---
+++ tennis-ball-detector/detectorLive.cpp
@@ -0,0 +1,36 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    string classifier = "data/cascade.xml";
+
+    CascadeClassifier ballCascade;
+    ballCascade.load(classifier);
+
+    Camera cam(0);
+
+    while(1)
+    {
+        Mat img, imgGray;
+        img = cam.getFrame();
+        cvtColor(img, imgGray, COLOR_BGR2GRAY);
+
+        vector<Rect> balls;
+        ballCascade.detectMultiScale(imgGray, balls, 2, 2);
+
+        for (int i = 0; i < balls.size(); i++)
+        {
+            rectangle(img, balls[i].tl(), balls[i].br(), Scalar(255, 0, 255), 3);
+        }
+
+        imshow("found balls", img);
+        waitKey(1);
+    }
+}

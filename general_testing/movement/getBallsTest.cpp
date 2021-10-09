8dd70fb4b6049ae156718f934dc5ec0247db7246
Acloran <andrew@cloran.com>
2021-10-09 13:19:34 -0500

    COLLECT 10 BALLS

--- general_testing/movement/getBallsTest.cpp
+++ general_testing/movement/getBallsTest.cpp
@@ -25,18 +25,23 @@ int main()
 {
     Robot r(LEFT_MOTOR_DIR_PIN, LEFT_MOTOR_POWER_PIN, RIGHT_MOTOR_DIR_PIN, RIGHT_MOTOR_POWER_PIN, INITIAL_X, INITIAL_Y, INITIAL_THETA, L_ENCODER_PIN1, L_ENCODER_PIN2, R_ENCODER_PIN1, R_ENCODER_PIN2, COUNT_INPUT_PIN, COLLECTOR_PIN);
     //setMask();
-    std::vector<cv::Rect2d> boxes = getBoundingBoxes();
-    while (boxes.size() == 0) //while we don't see any balls
+    int count = 0;
+    while(count++ < 10)
     {
+        std::vector<cv::Rect2d> boxes = getBoundingBoxes();
+        while (boxes.size() == 0) //while we don't see any balls
+        {
+            boxes = getBoundingBoxes();
+            r.setMotorPowers(.2, -.2);
+        }
+        r.setMotorPowers(0, 0);
+
+        cv::waitKey(200);
         boxes = getBoundingBoxes();
-        r.setMotorPowers(.2, -.2);
+
+        //now that we see a ball on the right of the screen, drive to it
+        //r.turnPixel(320, .5, .12, boxes.at(0));
+        r.curveToBall(boxes.at(0), .37, .12);
     }
-    r.setMotorPowers(0, 0);
-
-    cv::waitKey(200);
-    boxes = getBoundingBoxes();
-
-    //now that we see a ball on the right of the screen, drive to it
-    //r.turnPixel(320, .5, .12, boxes.at(0));
-    r.curveToBall(boxes.at(0), .37, .12);
+    
 }

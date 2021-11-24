b03535e9ecc11c0c919fb3462f6714e377b136d5
droiddoes9 <drewmca04@gmail.com>
2021-11-23 20:43:16 -0600

    remove duplicate folders

--- haar_cascade_detector/detector_live.py
+++ haar_cascade_detector/detector_live.py
@@ -7,8 +7,9 @@ import math
 # Run Command:
 #python3 DetectorRealTime.py data/cascade.xml
 
+classifier = sys.argv[1]
 
-classifier = cv2.CascadeClassifier("data/cascade.xml")
+classifier = cv2.CascadeClassifier(classifier)
 
 
 fpsLimit = 1 # throttle limit
@@ -16,7 +17,7 @@ startTime = time.time()
 cap = cv2.VideoCapture(0)
 cap.set(4, 720)
 cap.set(3, 1280)
-cap.set(cv2.CAP_PROP_EXPOSURE, -3)
+
 
 while True:
     ret_val, img = cap.read()
@@ -26,15 +27,14 @@ while True:
     #frameRate = 1/(nowTime - startTime)
     #print("FPS:",frameRate, end="\r")
     img_gray = cv2.cvtColor(cv2.UMat(img), cv2.COLOR_BGR2GRAY)
-    img_HSV = cv2.cvtColor(cv2.UMat(img), cv2.COLOR_BGR2HSV)
-    found = classifier.detectMultiScale(img, minSize =(10, 10))
+    found = classifier.detectMultiScale(img_gray, minSize =(10, 10))
     if(len(found)!=0):
         
             for i, (x, y, width, height) in enumerate(found):
-                cv2.rectangle(img_HSV, (x, y), (x + height, y + width), (0, 255, 0), 5)
+                cv2.rectangle(img, (x, y), (x + height, y + width), (0, 255, 0), 5)
                 #inches = round(((133.5879*math.e)**(-0.0129*height)), 1)
                 print((2.65*1350)/height,"Inches",end="\n")
-    cv2.imshow("output", img_HSV)
+    cv2.imshow("output", img)
     if cv2.waitKey(1) == 27: 
         break  # esc to quit
     #startTime = time.time() # reset time
